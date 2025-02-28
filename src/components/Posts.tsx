fd51ac3d8721d053ac87fb523ab754ee1f8c0636
sergei <ravinski.sergei@gmail.com>
2025-02-28 13:16:44 +0100

    Initial commit

---
+++ src/components/Posts.tsx
@@ -0,0 +1,22 @@
+// src/components/Posts.tsx
+
+import { POSTS_QUERYResult } from "../../sanity.types";
+
+export function Posts({ posts }: { posts: POSTS_QUERYResult }) {
+	console.log(posts);
+
+	return (
+		<ul className="container mx-auto grid grid-cols-1 divide-y divide-blue-100">
+			{posts.map((post) => (
+				<li key={post._id}>
+					<a
+						className="block p-4 hover:bg-blue-50"
+						href={`/posts/${post?.slug?.current}`}
+					>
+						{post?.title}
+					</a>
+				</li>
+			))}
+		</ul>
+	);
+}
