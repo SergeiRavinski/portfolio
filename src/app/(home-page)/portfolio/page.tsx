98e6423a551b00f534e8af7f551d73f12e32f06f
sergei <ravinski.sergei@gmail.com>
2025-03-10 20:07:54 +0100

    Portfolio page

--- src/app/(home-page)/portfolio/page.tsx
+++ src/app/(home-page)/portfolio/page.tsx
@@ -1,18 +1,33 @@
 import Header from "@/components/ui/Header";
 import { sanityFetch } from "@/sanity/lib/live";
 import { PROJECTS_QUERY } from "@/sanity/lib/queries";
+import Link from "next/link";
 
 export default async function Portfolio() {
-	const data = await sanityFetch({ query: PROJECTS_QUERY });
+	const { data } = await sanityFetch({ query: PROJECTS_QUERY });
 
 	return (
 		<section className="flex flex-col min-h-screen w-full">
 			<Header />
 
 			<main className="container mx-auto bg-green-200 p-6 w-full h-full">
-				{data?.data?.map((item: { _id: number; title: string }) => {
-					return <h1 key={item._id}>{item.title}</h1>;
-				})}
+				{data && (
+					<section className="flex flex-col">
+						{data?.map(
+							(item: {
+								link: string;
+								_id: number;
+								title: string;
+							}) => {
+								return (
+									<Link href={item.link} key={item._id}>
+										{item.title}
+									</Link>
+								);
+							}
+						)}
+					</section>
+				)}
 			</main>
 		</section>
 	);
