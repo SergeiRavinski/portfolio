ae238812c0f064038ac6d1ca2bdbe373a7374038
sergei <ravinski.sergei@gmail.com>
2025-03-10 20:00:39 +0100

    Projects query

--- src/app/(home-page)/portfolio/page.tsx
+++ src/app/(home-page)/portfolio/page.tsx
@@ -1,18 +1,18 @@
 import Header from "@/components/ui/Header";
-// import { sanityFetch } from "@/sanity/lib/live";
-// import { POSTS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch } from "@/sanity/lib/live";
+import { PROJECTS_QUERY } from "@/sanity/lib/queries";
 
-export default async function MainSection() {
-	// const { data: posts } = await sanityFetch({
-	// 	query: POSTS_QUERY,
-	// });
+export default async function Portfolio() {
+	const data = await sanityFetch({ query: PROJECTS_QUERY });
 
 	return (
 		<section className="flex flex-col min-h-screen w-full">
 			<Header />
 
 			<main className="container mx-auto bg-green-200 p-6 w-full h-full">
-				<h1>Add my projects here ...</h1>
+				{data?.data?.map((item: { _id: number; title: string }) => {
+					return <h1 key={item._id}>{item.title}</h1>;
+				})}
 			</main>
 		</section>
 	);
--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -9,3 +9,15 @@ export const POST_QUERY =
 	defineQuery(`*[_type == "post" && slug.current == $slug][0]{
   title, body, mainImage
 }`);
+
+export const PROJECTS_QUERY = defineQuery(`*[_type == 'projects'] {
+	_id,
+	title,
+	image,
+	date,
+	description,
+	link,
+	frontendTechnologies,
+	backendTechnologies,
+	tools
+  }`);
