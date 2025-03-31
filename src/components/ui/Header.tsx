07af4712023d14ee25072a8b82b0f694cfe70ea6
sergei <ravinski.sergei@gmail.com>
2025-03-31 14:25:52 +0200

    Header styling

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -6,33 +6,38 @@ import { usePathname } from "next/navigation";
 export default function Header() {
 	const path = usePathname();
 
+	const styleActive = "border-b-2 font-bold border-[#29292B]";
+
 	return (
 		<header className="container mx-auto w-full p-6">
-			<ul className="flex flex-row gap-3 justify-self-end border-b-2 border-[#18161333] uppercase">
-				<li
-					className={`${path === "/portfolio" ? "border-b-2 border-black" : ""}`}
-				>
-					<Link href="/portfolio">Portfolio</Link>
-				</li>
+			<nav>
+				<ul className="flex flex-row gap-3 justify-self-end border-b-2 border-[#D2D2D5] text-xl">
+					{/* TODO: Add active class to the current page using ref */}
+					<li
+						className={`${path === "/portfolio" ? styleActive : ""}`}
+					>
+						<Link href="/portfolio">
+							{path === "/portfolio" ? "." : ""}Portfolio
+						</Link>
+					</li>
 
-				<li
-					className={`${path === "/resume" ? "border-b-2 border-black" : ""}`}
-				>
-					<Link href="/resume">Resume</Link>
-				</li>
+					<li className={`${path === "/resume" ? styleActive : ""}`}>
+						<Link href="/resume">
+							{path === "/resume" ? "." : ""}Resume
+						</Link>
+					</li>
 
-				<li
-					className={` pb-2 ${path === "/" ? "border-b-2 border-black" : ""}`}
-				>
-					<Link href="/">About</Link>
-				</li>
+					<li className={` pb-2 ${path === "/" ? styleActive : ""}`}>
+						<Link href="/">{path === "/" ? "." : ""}About</Link>
+					</li>
 
-				<li
-					className={`${path === "/contact" ? "border-b-2 border-black" : ""}`}
-				>
-					<Link href="/contact">Contact</Link>
-				</li>
-			</ul>
+					<li className={`${path === "/contact" ? styleActive : ""}`}>
+						<Link href="/contact">
+							{path === "/contact" ? "." : ""}Contact
+						</Link>
+					</li>
+				</ul>
+			</nav>
 		</header>
 	);
 }
