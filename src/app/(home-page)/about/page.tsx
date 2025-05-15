8fc2b7c68e5bdb15af7183e3ae84bb5c115e8fee
sergei <ravinski.sergei@gmail.com>
2025-05-15 14:40:14 +0200

    The About page

--- src/app/(home-page)/about/page.tsx
+++ src/app/(home-page)/about/page.tsx
@@ -1,12 +1,17 @@
 import { Posts } from "@/components/pages/home-page/Posts";
 import SplitText from "@/components/pages/portfolio/TitleAnimation";
+import Button from "@/components/ui/Button";
 import { sanityFetch } from "@/sanity/lib/live";
 import { POSTS_QUERY } from "@/sanity/lib/queries";
+import Image from "next/image";
 
 export default async function About() {
-	const { data: posts } = await sanityFetch({
-		query: POSTS_QUERY,
-	});
+	// const { data: posts } = await sanityFetch({
+	// 	query: POSTS_QUERY,
+	// });
+	const sendEmail = () => {
+		return console.log("Send email");
+	};
 
 	return (
 		<>
@@ -14,9 +19,40 @@ export default async function About() {
 				<SplitText text="About" />
 			</div>
 
-			<main className="container relative mx-auto w-full h-full overflow-scroll px-6 hide-scrollbar">
-				<Posts posts={posts} />
-			</main>
+			<section className="flex flex-row w-full text-(--color-primary-light) h-100 bg-(--color-primary-dark)">
+				<div className="flex flex-col justify-center w-1/2 h-full p-6">
+					<h1>
+						Sergei Ravinski Frontend / Fullstack Utvikler | Next.js
+						| Sanity
+					</h1>
+
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Quisquam, voluptatibus. Lorem ipsum dolor sit amet
+						consectetur adipisicing elit. Quisquam, voluptatibus.
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Quisquam, voluptatibus.
+					</p>
+
+					<section className="flex mt-6">
+						<Button
+							// clickEvent={sendEmail}
+							type={"text"}
+							text={"Send en e-post"}
+						/>
+					</section>
+				</div>
+
+				<div className="flex w-1/2 h-full">
+					<Image
+						className="dark:invert w-full object-cover"
+						src={"/kristiania.png"}
+						width={250}
+						height={250}
+						alt="Default icon"
+					/>
+				</div>
+			</section>
 		</>
 	);
 }
