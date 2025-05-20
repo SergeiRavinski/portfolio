00aa490d2a10e266b2ddbbf3053cf6c5ec04b4ed
sergei <ravinski.sergei@gmail.com>
2025-05-20 14:41:17 +0200

    Layouts for the each route

---
+++ src/app/(about-page)/layout.tsx
@@ -0,0 +1,30 @@
+import { SanityLive } from "@/sanity/lib/live";
+import { DisableDraftMode } from "@/components/DisableDraftMode";
+import { VisualEditing } from "next-sanity";
+import { draftMode } from "next/headers";
+import Aside from "@/components/ui/Aside";
+import MainContentWrapper from "@/components/global/MainContentWrapper";
+import Header from "@/components/ui/Header";
+import RootWrapper from "@/components/global/RootWrapper";
+
+export default async function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<RootWrapper>
+			<MainContentWrapper>
+				<Header />
+				{children}
+			</MainContentWrapper>
+			<SanityLive />
+			{(await draftMode()).isEnabled && (
+				<>
+					<DisableDraftMode />
+					<VisualEditing />
+				</>
+			)}
+		</RootWrapper>
+	);
+}
