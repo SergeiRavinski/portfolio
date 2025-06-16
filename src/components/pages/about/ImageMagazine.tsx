a6045c8ed701d445f2bb26e48f6a8f60fa450100
sergei <ravinski.sergei@gmail.com>
2025-06-16 15:20:53 +0200

    Create ImageMagazine component

---
+++ src/components/pages/about/ImageMagazine.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import { ImageMagazineProps } from "@/types/about-page";
+
+export default function ImageMagazine(props: ImageMagazineProps) {
+	const { src, altText, size, hoverElement, hoverText } = props || {};
+	const hoverElementStyles =
+		hoverElement &&
+		"relative group transition-all duration-300 ease-in-out";
+	const styles =
+		size === "small"
+			? `h-full ${hoverElementStyles}`
+			: `h-full grid col-span-2 row-span-2 ${hoverElementStyles}`;
+
+	return (
+		<div className={styles}>
+			<Image
+				className="w-full h-full object-cover"
+				src={src}
+				width={500}
+				height={500}
+				alt={altText ? altText : "Image"}
+			/>
+
+			{/* Hover text element */}
+			{hoverElement && (
+				<div className="hidden group-hover:flex h-full w-full text-(--color-primary-light) p-6 absolute top-0 left-0 bg-(--color-primary-dark) opacity-80 transition-all duration-300 ease-in-out">
+					{hoverText}
+				</div>
+			)}
+		</div>
+	);
+}
