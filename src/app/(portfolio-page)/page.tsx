115c1c86d9bddebfec76a70d3e604cc8baef9bdc
sergei <ravinski.sergei@gmail.com>
2025-08-29 14:12:35 +0200

    Add try catch for the error handling in portfolio page

--- src/app/(portfolio-page)/page.tsx
+++ src/app/(portfolio-page)/page.tsx
@@ -1,10 +1,22 @@
 import MainSection from "@/components/ui/MainSection";
+
 import { sanityFetch } from "@/sanity/lib/live";
 import { PROJECTS_QUERY } from "@/sanity/lib/queries";
 import { algoliasearch } from "algoliasearch";
 
 export default async function Page() {
 	const { data } = await sanityFetch({ query: PROJECTS_QUERY });
+
+	try {
+		if (!data) {
+			throw new Error("No data found");
+		}
+
+		return <MainSection data={data} />;
+	} catch (error) {
+		console.error("Error fetching data:", error);
+	}
+
 	const client = algoliasearch(
 		process.env.NEXT_PUBLIC_ALGOLIA_APP_ID as string,
 		process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_KEY as string
@@ -21,6 +33,4 @@ export default async function Page() {
 	processRecords()
 		.then(() => console.log("Successfully indexed objects!"))
 		.catch((err) => console.error(err));
-
-	return <MainSection data={data} />;
 }
