df96f5a2e8ab1ffe8c7b8a114805b11be11fd1ea
sergei <ravinski.sergei@gmail.com>
2025-08-29 14:11:43 +0200

    Add link to Button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Button({
 	clickEvent,
@@ -6,23 +7,23 @@ export default function Button({
 	text,
 	color,
 	changeStyling,
+	link,
+	target,
 }: {
 	clickEvent?: () => void;
 	type?: string;
 	text?: string;
 	color?: string;
 	changeStyling?: boolean;
+	link?: string;
+	target?: string;
 }) {
 	const buttonType = () => {
 		switch (type) {
 			case "styling":
 				return getIcon();
 			case "text":
-				return (
-					<h3 className="group-hover:text-(--color-tertiary-dark) transition-all duration-300">
-						{text}
-					</h3>
-				);
+				return <h3>{text}</h3>;
 			default:
 				return null;
 		}
@@ -48,7 +49,20 @@ export default function Button({
 		);
 	};
 
-	return (
+	return link ? (
+		<Link href={link} target={target ? "_blank" : ""}>
+			<button
+				type="submit"
+				onClick={clickEvent}
+				className={`flex justify-center group normal-case p-2 items-center relative border-1 group border-solid transition-border duration-300 rounded-xs ${color === "dark" ? "bg-(--color-primary-dark) text-(--color-primary-light) border-white hover:border-(--color-dark-hover)" : "border-(--color-secondary-dark) hover:border-(--color-dark-hover)"} `}
+			>
+				{buttonType()}
+
+				<div className="absolute left-0 top-1/2 -translate-y-2/4 -translate-x-2/4 bg-(--color-tertiary-dark) w-[1px] h-[40%] group-hover:h-[60%]"></div>
+				<div className="absolute right-0 top-1/2 -translate-y-2/4 translate-x-2/4 bg-(--color-tertiary-dark) w-[1px] h-[40%] group-hover:h-[60%] transition-h duration-300"></div>
+			</button>
+		</Link>
+	) : (
 		<button
 			type="submit"
 			onClick={clickEvent}
