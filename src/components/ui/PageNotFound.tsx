54a83f102424ac6ddc5c32b21278185dc1212922
sergei <ravinski.sergei@gmail.com>
2025-08-29 14:09:48 +0200

    Create PageNotFound component for 404 error handling

---
+++ src/components/ui/PageNotFound.tsx
@@ -0,0 +1,25 @@
+import MainContentWrapper from "../global/MainContentWrapper";
+import RootWrapper from "../global/RootWrapper";
+import Button from "./Button";
+import Header from "./Header";
+import SplitTextYoyo from "./SplitTextYoyo";
+
+export default function PageNotFound() {
+	return (
+		<RootWrapper>
+			<MainContentWrapper>
+				<Header />
+
+				<main className="flex z-50 h-full flex-col items-center justify-center bg-(--color-primary-light) px-6 text-center text-(--color-primary-dark)">
+					<SplitTextYoyo text="404" isNotFound />
+
+					<p className="mb-8 max-w-md text-lg text-(--color-primary-dark) sm:text-xl">
+						Oops. This page doesn’t exist.
+					</p>
+
+					<Button text="Back to Home" type="text" link="/" />
+				</main>
+			</MainContentWrapper>
+		</RootWrapper>
+	);
+}
