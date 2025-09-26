f464718a57bf8d84a0ecd47f719f2cf9ac74c20d
Sergei_ravinski <Ravinski.sergei@gmail.com>
2025-09-26 12:46:02 +0200

    Chore/remove algolia related components (#17)
    * Chore/remove Algolia integration and unused search-update route
    
    * Chore/remove unused configuration and styling files

--- src/app/(portfolio-page)/page.tsx
+++ src/app/(portfolio-page)/page.tsx
@@ -1,7 +1,6 @@
 import MainSection from "@/components/pages/portfolio/MainSection";
 import { sanityFetch } from "@/sanity/lib/live";
 import { PROJECTS_QUERY } from "@/sanity/lib/queries";
-import { algoliasearch } from "algoliasearch";
 
 export default async function Page() {
 	const { data } = await sanityFetch({ query: PROJECTS_QUERY });
@@ -15,21 +14,4 @@ export default async function Page() {
 	} catch (error) {
 		console.error("Error fetching data:", error);
 	}
-
-	const client = algoliasearch(
-		process.env.NEXT_PUBLIC_ALGOLIA_APP_ID as string,
-		process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_KEY as string
-	);
-
-	// Fetch and index objects in Algolia
-	const processRecords = async () => {
-		return await client.saveObjects({
-			indexName: "movies_index",
-			objects: data,
-		});
-	};
-
-	processRecords()
-		.then(() => console.log("Successfully indexed objects!"))
-		.catch((err) => console.error(err));
 }
