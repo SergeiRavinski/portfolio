9ecac4e6fa280d60230aaf619691dbba0ccf2fbc
sergei <ravinski.sergei@gmail.com>
2025-10-07 16:01:34 +0200

    Fetch about data

--- src/app/(about-page)/about/page.tsx
+++ src/app/(about-page)/about/page.tsx
@@ -1,34 +1,40 @@
 import Magazine from "@/components/pages/about/Magazine";
 import PromoBlock from "@/components/pages/about/PromoBlock";
 import SplitTextYoyo from "@/components/ui/SplitTextYoyo";
+import { sanityFetch } from "@/sanity/lib/live";
+import { ABOUT_QUERY } from "@/sanity/lib/queries";
 
 export default async function About() {
-	return (
-		<>
-			<div className="p-6 flex flex-row justify-between items-center">
-				<SplitTextYoyo text={"About"} />
-			</div>
+	const { data } = await sanityFetch({ query: ABOUT_QUERY });
+	const {
+		// _id,
+		title,
+		// name,
+		magazineComponents,
+		promoBlock,
+		// phone,
+		// email,
+		// links,
+		// cv,
+		// location,
+	} = data || {};
 
-			<Magazine />
-			<PromoBlock
-				title="Sergei Ravinski – Frontend / Fullstack utvikler"
-				text="E-post: test+test@gmail.com
-				Telefon: +47 02985294
-				Adresse: Oslo
-				LinkedIn: linkedin.com/in/brukernavn
-				GitHub: github.com/brukernavn"
-				layout="imageLeft"
-				background="light"
-				button={{
-					type: "text",
-					textButton: "Ta kontakt",
-					link: "/contact",
-				}}
-				image={{
-					src: "/developer-black.svg",
-					alt: "Ikone av en utvikler",
-				}}
-			/>
-		</>
-	);
+	try {
+		if (!data) {
+			throw new Error("No data found");
+		}
+
+		return (
+			<>
+				<div className="p-6 flex flex-row justify-between items-center">
+					<SplitTextYoyo text={title} />
+				</div>
+
+				<Magazine magazineData={magazineComponents} />
+				<PromoBlock promoBlockData={promoBlock} />
+			</>
+		);
+	} catch (error) {
+		console.error("Error fetching data:", error);
+	}
 }
