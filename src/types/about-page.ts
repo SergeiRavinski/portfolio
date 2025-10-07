7d0ee6186352a410bcc883898ed76f26fe7d7513
sergei <ravinski.sergei@gmail.com>
2025-10-07 15:57:50 +0200

    Types

--- src/types/about-page.ts
+++ src/types/about-page.ts
@@ -1,3 +1,6 @@
+import { ReactNode } from "react";
+import { PortableTextBlock } from "sanity";
+
 export interface ImageMagazineProps {
 	src: string;
 	altText: string;
@@ -9,13 +12,14 @@ export interface ImageMagazineProps {
 export interface TextMagazineProps {
 	size: "small" | "large";
 	sectionTitle?: string;
-	text: string;
+	text: PortableTextBlock[];
+	theme: "light" | "dark";
 }
 
 export interface MapMagazineProps {
 	lng: number;
 	lat: number;
-	map: "light" | "streets";
+	map: "light" | "streetsv12";
 }
 
 export interface PromoBlockMagazineProps {
@@ -24,21 +28,106 @@ export interface PromoBlockMagazineProps {
 	size: "small" | "large";
 	sectionTitle?: string;
 	title?: string;
-	text?: string;
+	text: PortableTextBlock[];
 }
 
 export interface PromoBlockProps {
-	title: string;
-	text: string;
-	layout: "imageLeft" | "imageRight";
-	background: "dark" | "light";
-	button: {
-		type: "text" | "primary" | "secondary";
-		textButton?: string;
-		link?: string;
+	promoBlockData: {
+		title?: string;
+		text: PortableTextBlock[];
+		layout: "imageLeft" | "imageRight";
+		background: "dark" | "light";
+		button: {
+			textButton?: string;
+			link?: string;
+		};
+		image: {
+			src: string;
+			alt: string;
+		};
+		imageUrl: string;
+		imageAlt: string;
 	};
-	image: {
-		src: string;
-		alt: string;
+}
+
+export interface MagazineDataProps {
+	magazineData: (
+		| {
+				_key: string;
+				_type: "imageObject";
+				image: {
+					src: string;
+					altText: string;
+				};
+				imageUrl: string;
+				imageAlt: string;
+				size: "small" | "large";
+				hoverElement?: boolean;
+				hoverText?: string;
+		  }
+		| {
+				_key: string;
+				_type: "promoBlockMagazineObject";
+				title?: string;
+				sectionTitle?: string;
+				image: {
+					src: string;
+					altText: string;
+				};
+				imageUrl: string;
+				imageAlt: string;
+				size: "small" | "large";
+				text: PortableTextBlock[];
+		  }
+		| {
+				_key: string;
+				_type: "textMagazineObject";
+				sectionTitle?: string;
+				text: PortableTextBlock[];
+				size: "small" | "large";
+				theme: "light" | "dark";
+		  }
+		| {
+				_key: string;
+				_type: "mapMagazineObject";
+				location: {
+					lng: number;
+					lat: number;
+				};
+				mapStyle: "light" | "streetsv12";
+		  }
+	)[];
+}
+
+export interface SanityImageValue {
+	value: {
+		_type: "image";
+		asset: {
+			_ref: string;
+			_type: "reference";
+		};
+		alt?: string;
+		caption?: string;
+	};
+}
+
+export interface PortableTextBlockValue {
+	children: ReactNode;
+	value?: PortableTextBlock;
+}
+
+export interface YouTubeValue {
+	value: {
+		_type: "youtube";
+		url: string;
+	};
+}
+
+export interface LinkValue {
+	children: React.ReactNode;
+	value: {
+		_type: "link";
+		href: string;
+		blank?: boolean;
 	};
 }
