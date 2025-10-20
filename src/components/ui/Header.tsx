54f56f1ab008d3df322a95e260624cb0b9355b38
sergei <ravinski.sergei@gmail.com>
2025-10-20 16:24:37 +0200

    Update Header component for improved layout and responsiveness

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -2,22 +2,23 @@
 
 import ThemeToggle from "@/theme/theme-toggle";
 import FlipLink from "../pages/portfolio/FlipLink";
-import HamburgerMenu from "./HamburgerMenu";
 import { usePathname } from "next/navigation";
 
 export default function Header() {
 	const path = usePathname();
+	const isPortfolioPage = path === "/";
 
 	return (
 		<header
-			className={`mx-auto w-full items-center py-6 flex ${path !== "/" ? "justify-between" : "justify-end"}`}
+			className={`mx-auto w-full items-center py-6 flex ${isPortfolioPage ? "justify-between lg:justify-end" : "justify-between"}`}
 		>
-			{path !== "/" && (
-				<section className="flex flex-row items-center text-xl tracking-tight gap-6">
-					{path === "/" && <HamburgerMenu />}
-					<ThemeToggle />
-				</section>
-			)}
+			{/* {path !== "/" && ( */}
+			<section
+				className={`${isPortfolioPage ? "flex lg:hidden" : "flex"} flex-row items-center text-xl tracking-tight gap-6`}
+			>
+				<ThemeToggle />
+			</section>
+			{/* )} */}
 
 			<nav className="justify-center h-fit">
 				<ul className="flex flex-row md:gap-5 gap-3 align-middle items-center justify-self-end border-(--color-secondary-dark) md:text-xl text-[1rem] tracking-tight">
