6d49a345f401c354249fb84e80174dff04e4cd08
sergei <ravinski.sergei@gmail.com>
2025-10-20 16:23:42 +0200

    Enhance SocialMedia and SplitTextYoyo components for responsive design

--- src/components/ui/SocialMedia.tsx
+++ src/components/ui/SocialMedia.tsx
@@ -4,14 +4,14 @@ import { usePathname } from "next/navigation";
 import { LinkProps } from "@/types/portfolio-page";
 import LinkComponent from "./LinkComponent";
 
-export default function SocialMedia({ links }: LinkProps) {
+export default function SocialMedia({ links, isDashed }: LinkProps) {
 	const pathname = usePathname();
 	const isPortfolioPage = pathname === "/";
 	const isContactPage = pathname === "/contact";
 
 	return (
 		<span
-			className={`flex flex-col text-[0.8rem] w-full gap-y-10 ${isPortfolioPage ? "mb-6 pr-4 ml-6" : ""}`}
+			className={`flex flex-col text-[0.8rem] w-full gap-y-10 ${isPortfolioPage ? "mb-6 md:pr-4 pr-0 md:ml-6 ml-0 md:mt-0 mt-4" : ""}`}
 		>
 			<section className="flex flex-col justify-center uppercase text-[0.8rem] gap-1">
 				{links.map((link) => {
@@ -20,7 +20,7 @@ export default function SocialMedia({ links }: LinkProps) {
 							key={link._key}
 							url={link.url}
 							title={link.title}
-							isDashed={isContactPage ? true : false}
+							isDashed={isContactPage || isDashed ? true : false}
 						/>
 					);
 				})}
