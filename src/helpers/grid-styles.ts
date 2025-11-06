f22d4b65be2c6ca59096aa1d91fd032a0d098db7
sergei <ravinski.sergei@gmail.com>
2025-11-06 16:17:17 +0100

    Refactor grid styles

--- src/helpers/grid-styles.ts
+++ src/helpers/grid-styles.ts
@@ -4,7 +4,8 @@ export const gridStyles: GridStyles[] = [
 	{
 		gridColumnStart: "md:col-start-1 col-start-1",
 		gridColumnEnd: "md:col-end-6 col-end-1",
-		size: "md:h-[60vh] h-full",
+		size: "h-full",
+		// size: "md:h-[60vh] h-full",
 		// transform: "transform translate-y-[0vw]",
 		// startPosition: "opacity-0",
 		// endPosition: "opacity-100",
@@ -13,7 +14,8 @@ export const gridStyles: GridStyles[] = [
 	{
 		gridColumnStart: "md:col-start-6 col-start-1",
 		gridColumnEnd: "md:col-end-11 col-end-1",
-		size: "md:h-[40vh] h-full md:mt-[10vh] mt-0",
+		size: "h-full md:mt-[10vh] mt-0",
+		// size: "md:h-[40vh] h-full md:mt-[10vh] mt-0",
 		// zIndex: "z-10",
 		// transform: "transform translate-y-[8vw]",
 		// startPosition: "opacity-0",
@@ -23,7 +25,8 @@ export const gridStyles: GridStyles[] = [
 	{
 		gridColumnStart: "md:col-start-1 col-start-1",
 		gridColumnEnd: "md:col-end-5 col-end-1",
-		size: "md:h-[36vw] h-full",
+		size: "h-full",
+		// size: "md:h-[36vw] h-full",
 		// transform: "transform translate-y-[-14vw]",
 		// startPosition: "opacity-0",
 		// endPosition: "opacity-100",
@@ -32,7 +35,8 @@ export const gridStyles: GridStyles[] = [
 	{
 		gridColumnStart: "md:col-start-5 col-start-1",
 		gridColumnEnd: "md:col-end-11 col-end-1",
-		size: "md:h-[26vw] h-full md:mt-[10vw] mt-0",
+		size: "h-full md:mt-[10vw] mt-0",
+		// size: "md:h-[26vw] h-full md:mt-[10vw] mt-0",
 		// transform: "transform translate-y-[-35vw]",
 		// startPosition: "opacity-0",
 		// endPosition: "opacity-100",
@@ -41,7 +45,8 @@ export const gridStyles: GridStyles[] = [
 	{
 		gridColumnStart: "md:col-start-4 col-start-1",
 		gridColumnEnd: "md:col-end-11 col-end-1",
-		size: "md:h-[32vw] h-full",
+		size: "h-full",
+		// size: "md:h-[32vw] h-full",
 		// transform: "transform translate-y-[-15vw] translate-x-[-10vw]",
 		// startPosition: "opacity-0",
 		// endPosition: "opacity-100",
@@ -50,7 +55,8 @@ export const gridStyles: GridStyles[] = [
 	{
 		gridColumnStart: "md:col-start-1 col-start-1",
 		gridColumnEnd: "md:col-end-5 col-end-1",
-		size: "md:h-[36vw] h-full",
+		size: "h-full",
+		// size: "md:h-[36vw] h-full",
 		// transform: "transform translate-y-[-32vw] translate-x-[5vw]",
 		// startPosition: "opacity-0",
 		// endPosition: "opacity-100",
