888942211db839042cbb69632756fd2df5a42101
Sergei_ravinski <Ravinski.sergei@gmail.com>
2025-11-14 10:48:05 +0100

    Feature/readme (#37)
    * Update package.json
    
    * Add favicon
    
    * Update README.md
    
    * Add Analytics component
    
    * Add email integration using SendGrid

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import ThemeProvider from "@/theme/theme-provider";
 import { Geist, Geist_Mono, Space_Mono } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -52,6 +53,7 @@ export default function RootLayout({
 				>
 					{children}
 					<SpeedInsights />
+					<Analytics />
 				</ThemeProvider>
 			</body>
 		</html>
