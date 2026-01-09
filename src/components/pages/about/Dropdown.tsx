661652f626823487b6ee20c422998b03f45eae53
sergei <ravinski.sergei@gmail.com>
2026-01-09 11:32:01 +0100

    Update Dropdown component to support isCollapsed prop

--- src/components/pages/about/Dropdown.tsx
+++ src/components/pages/about/Dropdown.tsx
@@ -6,19 +6,23 @@ import { PortableText } from "@portabletext/react";
 import Image from "next/image";
 import { useRef, useState } from "react";
 
-export default function Dropdown({ label, content }: DropdownProps) {
-	const [isOpen, setIsOpen] = useState(false);
+export default function Dropdown({
+	label,
+	content,
+	isCollapsed,
+}: DropdownProps) {
+	const [isOpen, setIsOpen] = useState(isCollapsed ? false : true);
 	const contentRef = useRef<HTMLDivElement>(null);
 
 	return (
 		<div className="relative flex flex-col h-full">
 			<button
 				onClick={() => setIsOpen(!isOpen)}
-				className="flex text-start w-full items-center md:text-[1.5rem] text-[1.25rem] font-semibold mt-6"
+				className="flex text-start justify-between w-full items-center mt-6 list-disc space-y-2 md:text-[1rem] text-[0.8rem] first:mt-4 font-bold"
 			>
 				{label}
 				<Image
-					className={`dark:invert ml-2 transition-transform duration-300 ${isOpen ? "rotate-45" : "rotate-0"} `}
+					className={`dark:invert ml-4 transition-transform duration-300 ${isOpen ? "rotate-45" : "rotate-0"} `}
 					src={"/plus.svg"}
 					width={35}
 					height={35}
@@ -32,9 +36,8 @@ export default function Dropdown({ label, content }: DropdownProps) {
 					maxHeight: isOpen
 						? `${contentRef.current?.scrollHeight}px`
 						: "0px",
-					borderBottomColor: "var(--color-primary-dark)",
 				}}
-				className="overflow-hidden transition-all pb-3 duration-300 border-b-[0.1rem] border-b-(--color-primary-dark)"
+				className="overflow-hidden transition-all pb-3 duration-300 border-b border-solid border-(--color-primary-dark)"
 			>
 				<PortableText
 					value={content}
